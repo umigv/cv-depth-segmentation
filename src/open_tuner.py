26b721cc0acde64c1f173a65903558bc01828922
the2nake <41810559+the2nake@users.noreply.github.com>
2026-03-30 17:48:37 -0400

    fix: update names in open_tuner.py

--- src/open_tuner.py
+++ src/open_tuner.py
@@ -9,14 +9,12 @@ from calibrate.ui import CameraUI
 import ransac as rsc
 
 
-
-
 def main():
 
     file = h5py.File("res/dual_camera_calibration.hdf5", "r")
     left = rsc.HDF5Source(file, 0)
     right = rsc.HDF5Source(file, 1)
-    
+
     conf = rsc.GridConfiguration(5000.0, 5000.0, 50.0)
     depseg = rsc.DepthSegementation([
         (left, rsc.CameraPosition(0, 0, 0)),
@@ -34,30 +32,30 @@ def main():
             right.use_frame(frame)
 
             frame += 1
-
+        depseg.timestamps = [0, 0]
         depseg.process()
 
-        depseg.sources = [
-            (depseg.sources[0][0], rsc.CameraPosition(
+        depseg._sources = [
+            (depseg._sources[0][0], rsc.CameraPosition(
                 10 * ui.params['left']['x_offset'],
                 10 * ui.params['left']['z_offset'],
                 math.radians(ui.params['left']['angle'])
             )),
-            (depseg.sources[1][0], rsc.CameraPosition(
+            (depseg._sources[1][0], rsc.CameraPosition(
                 10 * ui.params['right']['x_offset'],
                 10 * ui.params['right']['z_offset'],
                 math.radians(ui.params['right']['angle'])
             ))
         ]
-        
+
         close = ui.render(
             grids=depseg.grids,
-            merged=depseg.reduce(np.logical_xor) * 255
+            merged=depseg.merge_simple(np.logical_xor) * 255
         )
 
         if close:
             break
-    
-    
+
+
 if __name__ == '__main__':
     main()
