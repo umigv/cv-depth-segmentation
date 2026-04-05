0f210d5b7d8b22ba48f8761f3dab0750116ca871
the2nake <41810559+the2nake@users.noreply.github.com>
2026-04-04 20:21:40 -0400

    fix(live_single): set init parameters from serial number before modifying them

--- src/live_single.py
+++ src/live_single.py
@@ -14,12 +14,12 @@ import ransac as rsc
 
 def run_ransac_on_zed(cam_pos=rsc.CameraPosition(), serial_number=None):
     init = sl.InitParameters()
+    if serial_number is not None:
+        init.set_from_serial_number(serial_number)
     init.async_image_retrieval = True
     init.depth_mode = sl.DEPTH_MODE.NEURAL
     init.camera_resolution = sl.RESOLUTION.HD720
     init.camera_fps = 30  # The framerate is lowered to avoid any USB3 bandwidth issues
-    if serial_number is not None:
-        init.set_from_serial_number(serial_number)
 
     live = rsc.LiveSource(init, (720, 404))
     conf = rsc.GridConfiguration(5000.0, 5000.0, 50.0)
